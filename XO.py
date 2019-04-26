2bc4655dfdcd6ebe5544219db7dfe094f5e96cc8
Volodya <volodka.orlov@gmail.com>
2019-04-26 22:09:12 +0300

    How does the feeld looks now?

--- XO.py
+++ XO.py
@@ -6,57 +6,53 @@ field_size = int(input('Enter size of field:'))
 
 def matgen(field_size):
     k = []
-    i = 1
-    while i != field_size - 1:
-        for j in range(field_size):
-            k.append(j)
-            i += 1
-        print(i)
+    for i in range(1, field_size ** 2, field_size):
+        row = []
+        for j in range(i, i + field_size):
+            row.append(j)
+        k.append(row)
     return k
 
 
-print(matgen(field_size))
+k = matgen(field_size)
 
-# start_time = time.time()
-#
-#
-# def matprint(field_size):
-#     print('=' * (4 * field_size - 3))
-#     for i in range(field_size):
-#         for j in range(field_size):
-#             if j == field_size - 1:
-#                 print(k[i][j])
-#                 break
-#             print(k[i][j], end=' | ')
-#         print('=' * (4 * field_size - 3))
-#
-#
-# def addelem():
-#     z = 0
-#
-#     while True:
-#         if z % 2 == 0:
-#             print('=X=')
-#             x, y = int(input('enter i:')) - 1, int(input('enter j:')) - 1
-#             if k[x][y] != ' ':
-#                 print('This cell is already taken')
-#                 break
-#             k[x][y] = 'X'
-#             matprint()
-#             z += 1
-#
-#         elif z % 2 == 1:
-#             print('=O=')
-#             x, y = int(input('enter i:')) - 1, int(input('enter j:')) - 1
-#             if k[x][y] != ' ':
-#                 print('This cell is already taken')
-#                 break
-#             k[x][y] = 'O'
-#             matprint()
-#             z += 1
-#         if z == 9:
-#             print('===Draw===')
-#             return
-#
-#
-# matprint(field_size)
+
+def field_print(field_size):
+    global k
+    print('=' * (4 * field_size - 3))
+    for i in range(field_size):
+        for j in range(field_size):
+            if j == field_size - 1:
+                print(k[i][j])
+                break
+            print(k[i][j], end=' | ')
+        print('=' * (4 * field_size - 3))
+
+
+field_print(field_size)
+
+
+def addelem():
+    global k
+    z = 0
+
+    def some(xo):
+        print('=' + str(xo) + '=')
+        y = int(input('Number:'))
+        for i in range(field_size):
+            for j in range(field_size):
+                if k[i][j] == y:
+                    k[i][j] = str(xo)
+                    field_print(field_size)
+
+    while True:
+        if z % 2 == 0:
+            some('X')
+            z += 1
+
+        elif z % 2 == 1:
+            some('O')
+            z += 1
+
+
+addelem()
